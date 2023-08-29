16b5a1140023187081ac94e5068a14df8b07f4a1
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-29 14:26:15 -0300

    Limpando os arquivos do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './componentes/banner/banner';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Estou editando meu arquivo
-        </a>
-      </header>
     </div>
   );
 }
