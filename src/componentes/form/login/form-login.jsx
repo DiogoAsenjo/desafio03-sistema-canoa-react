4be766a54f8f9b77925c7a43ecd909469bea1774
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-30 16:49:46 -0300

    Usando o useState no form-login

--- src/componentes/form/login/form-login.jsx
+++ src/componentes/form/login/form-login.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Button } from '../../button/button'
 import { TextField } from '../../text-field/text-field'
 import './form-login.css'
@@ -6,15 +7,31 @@ export const FormLogin = () => {
 
     const login = (event) => {
         event.preventDefault();
-        console.log('Logged in!');
+        console.log(`${username} logged using this password: ${password}`);
     }
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <section className='form-login'>
             <form onSubmit={login}>
                 <h2>Login</h2>
-                <TextField mandatory = {true} label="Username (e-mail)" placeholder="example@email.com"/>
-                <TextField mandatory = {true} label="Password" placeholder="********"/>
+                <TextField 
+                    mandatory={true} 
+                    label="Username (e-mail)" 
+                    placeholder="example@email.com"
+                    value={username}
+                    typed={value => setUsername(value)}
+                />
+                <TextField 
+                    mandatory = {true} 
+                    label="Password" 
+                    placeholder="********"
+                    value={password}
+                    typed={value => setPassword(value)}
+                    type='password' 
+                />
                 <p>Remember: password should have at least 8 characters and one of each: lowercase, uppercase, a number and a symbol</p>
                 <Button>
                     Login
--- src/componentes/text-field/text-field.jsx
+++ src/componentes/text-field/text-field.jsx
@@ -1,17 +1,16 @@
+import { useState } from 'react'
 import './text-field.css'
 
 export const TextField = (props) => {
 
-    const placeholderData = `${props.placeholder}`
-
     const typingOnPlaceholder = (event) => {
-        console.log(event.target.value)
+        props.typed(event.target.value);
     }
 
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input onChange = {typingOnPlaceholder} required = {props.mandatory} placeholder={props.placeholder}/>
+            <input value = {props.value} onChange = {typingOnPlaceholder} required = {props.mandatory} placeholder = {props.placeholder} type = {props.type}/>
         </div>
     )
 }
