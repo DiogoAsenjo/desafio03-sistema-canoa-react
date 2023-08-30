40ccb37d2f7ceb11ed149a34243336fc5069743a
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-30 10:55:10 -0300

    Aprendendo a responder a eventos e deixando os campos de texto como de preenchimento obrigatório

--- src/componentes/form/form-create-account.jsx
+++ src/componentes/form/form-create-account.jsx
@@ -3,16 +3,22 @@ import { TextField } from '../text-field/text-field'
 import './form-create-account.css'
 
 export const FormCreateAccount = () => {
+
+    const createAccount = (event) => {
+        event.preventDefault();
+        console.log('Account created successfully');
+    }
+
     return (
         <section className='form-create-account'>
-            <form>
+            <form onSubmit={createAccount}>
                 <h2>Fill all the fields to create an account</h2>
-                <TextField label="Full Name" placeholder="Write your name"/>
-                <TextField label="Cellphone" placeholder="(XX)9XXXX-XXXX"/>
-                <TextField label="E-mail" placeholder="example@email.com"/>
-                <TextField label="Password" placeholder="Type your password"/>
+                <TextField mandatory = {true} label="Full Name" placeholder="Write your name"/>
+                <TextField mandatory = {true} label="Cellphone" placeholder="(XX)9XXXX-XXXX"/>
+                <TextField mandatory = {true} label="E-mail" placeholder="example@email.com"/>
+                <TextField mandatory = {true} label="Password" placeholder="Type your password"/>
                 <p>Password should have at least 8 characters and one of each: lowercase, uppercase, a number and a symbol</p>
-                <TextField label="Confirm password" placeholder="Type your password again"/>
+                <TextField mandatory = {true} label="Confirm password" placeholder="Type your password again"/>
                 <Button>
                     Create Account
                 </Button>
--- src/componentes/text-field/text-field.jsx
+++ src/componentes/text-field/text-field.jsx
@@ -4,7 +4,7 @@ export const TextField = (props) => {
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input required = {props.mandatory} placeholder={props.placeholder}/>
         </div>
     )
 }
