c416a67c2bb8f40501eb6e0f32886e7ffc1d1f32
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-30 14:13:59 -0300

    Entendendo sobre os estados dos componentes

--- src/componentes/text-field/text-field.jsx
+++ src/componentes/text-field/text-field.jsx
@@ -1,10 +1,17 @@
 import './text-field.css'
 
 export const TextField = (props) => {
+
+    const placeholderData = `${props.placeholder}`
+
+    const typingOnPlaceholder = (event) => {
+        console.log(event.target.value)
+    }
+
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input required = {props.mandatory} placeholder={props.placeholder}/>
+            <input onChange = {typingOnPlaceholder} required = {props.mandatory} placeholder={props.placeholder}/>
         </div>
     )
 }
