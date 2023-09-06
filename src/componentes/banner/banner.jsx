00ea6623ba36a31d1cfb10f3c701324a520e6305
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-06 13:07:21 -0300

    Finalizando o deploy, oremos

--- src/componentes/banner/banner.jsx
+++ src/componentes/banner/banner.jsx
@@ -1,9 +1,9 @@
-import './banner.css'
+import "./banner.css";
 
 export const Banner = () => {
-    return (
-        <header className = "banner">
-            <img src="/imagens/banner.png" alt="Banner principal da página"/>
-        </header>
-    )
-}
+  return (
+    <header className="banner">
+      <img src="./imagens/banner.png" alt="Banner principal da página" />
+    </header>
+  );
+};
