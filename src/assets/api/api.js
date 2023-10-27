01e1b8ae460a0a7647206079a59ef0a2f3d9b012
DiogoAsenjo <diogoasenjo@outlook.com>
2023-10-27 16:10:48 -0300

    Alterando a rota de conexão com a API

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://load-balancer-test-1847978165.us-east-2.elb.amazonaws.com",
+  baseURL: "https://load-balancer-test-1847978165.us-east-2.elb.amazonaws.com",
 });
