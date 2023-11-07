053d755c80377e45528344a8391749bced0b95ca
DiogoAsenjo <diogoasenjo@outlook.com>
2023-11-07 12:04:59 -0300

    Ajustando a conexao com a api

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://load-balancer-test-1847978165.us-east-2.elb.amazonaws.com",
+  baseURL: "https://asenjo.riann.dev/",
 });
